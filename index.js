319b0636b84329fb19134bf8827ac657f6373e44
Bridget Akellian <akellianbv@gmail.com>
2022-02-02 00:14:06 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+let multiply = num2 * num1
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 4
+let num4 = 42
+
+let mod = num3 % num4;
+
+
+
+let max = Math.max(5, 10, 15, 20);
